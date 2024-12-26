ecbffb1256360794845dbeb9b5802bfadda16875
Devansh Thakur <devanshthakur@Devanshs-MacBook-Air.local>
2024-12-27 00:36:39 +0530

    registration functionality

---
+++ src/main/java/com/dec24/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,9 @@
+package com.dec24.jobportal.repository;
+
+import com.dec24.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
---
+++ src/main/java/com/dec24/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,10 @@
+package com.dec24.jobportal.repository;
+
+import com.dec24.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+
+}
