bb1ccbdf1179761b5838a52d26e97ae6075e38b9
Devansh Thakur <devanshthakur@Devanshs-MacBook-Air.local>
2024-12-30 12:53:49 +0530

    job activity commit

---
+++ src/main/java/com/dec24/jobportal/services/JobSeekerProfileService.java
@@ -0,0 +1,27 @@
+package com.dec24.jobportal.services;
+
+import com.dec24.jobportal.entity.JobSeekerProfile;
+import com.dec24.jobportal.repository.JobSeekerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    @Autowired
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+        return jobSeekerProfileRepository.findById(id);
+    }
+
+    public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
+        return jobSeekerProfileRepository.save(jobSeekerProfile);
+    }
+}
